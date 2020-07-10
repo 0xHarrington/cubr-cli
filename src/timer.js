c4ca62782726e2453845b7a95cc9ac81889e1b29
Matt Harrington <harringtonswim@gmail.com>
2020-07-10 09:48:33 -0400

    timer window

--- src/timer.js
+++ src/timer.js
@@ -16,8 +16,23 @@ var times = contrib.table({
 
 times.setData({
   headers: ['Solve #', 'Time', 'Ao5', 'Ao12']
+})
 
 
 // grid.set(row#, col#, rowSpan, colSpan, obj, opts)
 var scramble = grid.set(1,1,1,3, contrib.markdown, {label: "Scramble"})
 var times = grid.set(2,1,2,1, contrib.table, {fg:"blue", label: "times"})
+var timer = grid.set(2,2,2,2, 
+
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// fixes https://github.com/yaronn/blessed-contrib/issues/10
+screen.on('resize', function() {
+  scramble.emit('attach');
+  times.emit('attach');
+});
+
+screen.render()
